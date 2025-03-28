564f9b6df0ddc18cf08149000eb2bd9768dc1732
thenry <thenry@student.42.fr>
2025-03-29 00:06:41 +0100

    both themes live

---
+++ matrix/js/components/help.js
@@ -0,0 +1,14 @@
+export function runHelp() {
+    return "\n" +
+            "welcome        - Shows the welcome message\n" +
+            "help           - Shows this help message\n" +
+            "status         - Shows my current status work-wise\n" +
+            "projects       - Displays my personal projects\n" +
+            "skills         - Displays my skills\n" +
+            "experiences    - Displays my work experiences\n" +
+            "education      - Displays my education\n" +
+            "about          - Displays more info about me\n" +
+            "contact        - Displays my contact information\n" +
+            "clear          - Clears the terminal output\n" + 
+            "\n";
+}
